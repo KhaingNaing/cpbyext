f4cebd7c9a4fd3b0d34303529fd377b214bba520
Khaing <khaing.naing@braineye.com>
2024-09-23 12:40:45 +1000

    adding fix to mulitple file extension copy

--- cpbyext.py
+++ cpbyext.py
@@ -7,38 +7,45 @@ from zipfile import ZipFile
 def main():
     args = sys.argv[1:]
     if args == []:
-        print('Usage: python3 cpbyext.py <ext>')
+        print('Usage: python3 cpbyext.py <ext1> <ext2> ...')
         sys.exit(1)
 
     # Counter for files copy
-    file_count = int()
+    total_file_count = int()
     
     # Set path to the current directory 
     path = Path(".")
+    temp_dir = tempfile.mkdtemp()
 
     for ext in args:
+        file_count = 0
         file_list = list(path.rglob("*." + ext))
 
         if not file_list:
             print(f"NO {ext.upper()} FILES TO COPY")
-        else:
-            temp_dir = tempfile.mkdtemp()
+            continue
 
-            for file in file_list:
-                f_path = Path(file)
-                shutil.copy(f_path, temp_dir)
-                file_count+=1
-            print(f"Copied {file_count} {ext.upper()} files")
-            zip_file_name = 'cpfiles.zip'
-            with ZipFile(zip_file_name, 'w') as myzip:
-                for root, dirnames, filenames in os.walk(temp_dir):
-                    for file in filenames:
-                        file_path = Path(root) / file                        
-                        arcname = os.path.relpath(file_path, start=temp_dir)
-                        myzip.write(file_path, arcname=arcname)
+        for file in file_list:
+            f_path = Path(file)
+            shutil.copy(f_path, temp_dir)
+            file_count+=1
+        print(f"Copied {file_count} {ext.upper()} files")
+        total_file_count += file_count
 
-            shutil.rmtree(temp_dir)
-            print(f"Files are copied to {zip_file_name}.")
+
+    zip_file_name = 'cpfiles.zip'
+    with ZipFile(zip_file_name, 'w') as myzip:
+        for root, dirnames, filenames in os.walk(temp_dir):
+            for file in filenames:
+                file_path = Path(root) / file                        
+                arcname = os.path.relpath(file_path, start=temp_dir)
+                myzip.write(file_path, arcname=arcname)
+
+    shutil.rmtree(temp_dir)
+    if total_file_count > 0:
+        print(f"Total {total_file_count} files copied and zipped into {zip_file_name}.")
+    else:
+        print("No files were copied.")
 
 if __name__ == "__main__":
     main()
